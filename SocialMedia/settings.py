44740da11abc045ad0f4797340fe14bfdd475988
Laser-Codes <lasercodeskaizen@gmail.com>
2023-04-11 16:03:14 +0530

    4th commit

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/"static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
