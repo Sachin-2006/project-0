810c8865141da91d1917aa0096e73fff6a09dbf2
Laser-Codes <lasercodeskaizen@gmail.com>
2023-04-12 15:02:19 +0530

    6th commit

--- post/models.py
+++ post/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 class Post(models.Model):
 	uploader = models.ForeignKey(User,on_delete = models.CASCADE)
@@ -9,6 +9,11 @@ class Post(models.Model):
 
 
 
-#class Comments(models.Model):
+class Comment(models.Model):
+	critic = models.ForeignKey(User,on_delete = models.CASCADE)
+	post = models.ForeignKey(Post,on_delete = models.CASCADE)
+	descr = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now())
+
 	
 		
